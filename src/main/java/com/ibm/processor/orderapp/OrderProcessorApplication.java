a652eca9fd628b2aa140c1bed9101129e37294d1
Todi <tbatchiev@gmail.com>
2025-04-10 21:09:50 +0300

    Rename classes. Add needed h2 config.

--- src/main/java/com/ibm/processor/orderapp/OrderProcessorApplication.java
+++ src/main/java/com/ibm/processor/orderapp/OrderProcessorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderappApplication {
+public class OrderProcessorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderappApplication.class, args);
+		SpringApplication.run(OrderProcessorApplication.class, args);
 	}
 
 }
